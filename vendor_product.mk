a048d9f1eaefed7005c4d3e4b66f364e16e689e8
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-09-06 21:59:24 +0000

    usb: Set the correct partition to ro.boot.usb prop
    [1] - https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/usb/-/blob/LA.VENDOR.13.2.0.r1-19400-KAILUA.0/vendor_product.mk?ref_type=tags
    
    Change-Id: Ia0e56c9eddc8461ca705406eed128bb678607395
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- vendor_product.mk
+++ vendor_product.mk
@@ -9,7 +9,7 @@ PRODUCT_PROPERTY_OVERRIDES += vendor.usb.dpl.inst.name=dpl
 
 ifneq ($(filter bengal monaco trinket,$(TARGET_BOARD_PLATFORM)),)
   PRODUCT_PROPERTY_OVERRIDES += vendor.usb.controller=4e00000.dwc3
-  PRODUCT_PROPERTY_OVERRIDES += ro.boot.usb.dwc3_msm=4e00000.ssusb
+  PRODUCT_SYSTEM_PROPERTIES += ro.boot.usb.dwc3_msm=4e00000.ssusb
 else ifneq ($(filter msm8998 sdm660,$(TARGET_BOARD_PLATFORM)),)
   PRODUCT_PROPERTY_OVERRIDES += vendor.usb.controller=a800000.dwc3
 else ifneq ($(filter msm8953,$(TARGET_BOARD_PLATFORM)),)
