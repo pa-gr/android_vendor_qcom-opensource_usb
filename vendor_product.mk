6c44cd58324f7213b8ff59bb60bf0ab111419e8e
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-09-06 21:53:48 +0000

    usb: Enable 4e00000.dwc3 props to all bengal variants
    [1] - Currently bengal is having the wrong definition of a600000.dwc3
    
    Change-Id: Iae9e4a9b3ac4785b83733c2ecdac2dd1e63c71a2
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- vendor_product.mk
+++ vendor_product.mk
@@ -7,7 +7,7 @@ PRODUCT_PROPERTY_OVERRIDES += vendor.usb.rmnet.func.name=gsi
 PRODUCT_PROPERTY_OVERRIDES += vendor.usb.rmnet.inst.name=rmnet
 PRODUCT_PROPERTY_OVERRIDES += vendor.usb.dpl.inst.name=dpl
 
-ifneq ($(filter bengal_515 monaco trinket,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter bengal monaco trinket,$(TARGET_BOARD_PLATFORM)),)
   PRODUCT_PROPERTY_OVERRIDES += vendor.usb.controller=4e00000.dwc3
   PRODUCT_PROPERTY_OVERRIDES += ro.boot.usb.dwc3_msm=4e00000.ssusb
 else ifneq ($(filter msm8998 sdm660,$(TARGET_BOARD_PLATFORM)),)
